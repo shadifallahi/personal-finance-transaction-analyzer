3d4fcbcb8ab5ad42bb649cc94beb50093820308b
shadifallahi <shadifallahi816@gmail.com>
2026-09-21 12:34:12 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
+import sqlite3
 
+def create_database():
+    connection = sqlite3.connect("finance.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT NOT NULL,
+            category TEXT NOT NULL,
+            amount REAL NOT NULL,
+            description TEXT NOT NULL
+        )
+    """)
+
+    connection.commit()
+    connection.close()
