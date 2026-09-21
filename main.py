50190e15b2441a824d71556e54a524acf0719e66
shadifallahi <shadifallahi816@gmail.com>
2026-09-21 12:27:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+transactions = []
 
+transaction = {
+    "type": "expense",
+    "category": "Food",
+    "amount": 25.50,
+    "description": "Lunch"
+}
+
+transactions.append(transaction)
+
+print("Transaction added!")
+print(transactions)
